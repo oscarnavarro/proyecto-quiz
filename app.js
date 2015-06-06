bd45e5038ed70cc1fabea514e16f10f13f783a65
oscar <racso188@hotmail.com>
2015-06-05 20:32:47 -0500

    Marco de la aplicación

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var partials = requeri('express-partials');
 var routes = require('./routes/index');
 
 
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(partials());
 app.use('/', routes);
 
 // catch 404 and forward to error handler
