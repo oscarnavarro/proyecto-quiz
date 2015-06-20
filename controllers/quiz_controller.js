a24ee0791cd395993f84c851e8be7592f312b3f5
oscar <racso188@hotmail.com>
2015-06-20 18:48:10 -0500

    finalizado modulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,17 +1,33 @@
 var models = require('../models/models.js');
 
-exports.question = function(req, res){
-  models.Quiz.findAll().success(function(quiz){
-    res.render('quizes/question', {pregunta: quiz[0].pregunta});
-  })
+exports.load = function (req, res, next, quizId) {
+  models.Quiz.find(quizId).then(
+    function(quiz){
+      if(quiz){
+        req.quiz = quiz;
+        next();
+      } else { next(new Error('No existe quizId'+ quizId));}
+    }
+  ).catch(function(error){next(error);});
+};
+
+exports.index = function (req, res) {
+  models.Quiz.findAll().then(function (quizes) {
+    res.render('quizes/index.ejs', {quizes:quizes});// body...
+  }
+).catch(function(error){next(error);});
+};
+
+exports.show = function(req, res){
+  res.render('quizes/show', {quiz: req.quiz});
 };
 
 exports.answer = function(req, res){
-  models.Quiz.findAll().success(function(quiz){
-    if (req.query.respuesta === quiz[0].respueta) {
-      res.render('quizes/answer', {respuesta: 'Correcto'});
-    } else {
-      res.render('quizes/answer', {respuesta: 'Incorrecto'});
-    }
-  })
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta === req.quiz.respuesta) {
+    resultado = 'Correcto';
+  }
+  console.log(req.quiz.respuesta);
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+
 };
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+router.param('quizId', quizController.load);
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 router.get('/author', function(req, res){
   res.render('author', {author: 'Oscar Navarro', foto:'images/foto.png'});
